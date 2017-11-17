79463edf0e503437095c41ce77c67e2436f35d59
codeshakti <codeshaktiman@gmail.com>
2017-11-17 16:19:31 +0100

    test deploy

--- client/react/src/services/awareApi.js
+++ client/react/src/services/awareApi.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 //const axios = require('axios')
 
 const awareApi = {
-	baseUrl: 'http://localhost:3005/charitysearch?user_key=01289bdc6c2b9417e0d77a5dc3a34112&searchTerm=india',
+	baseUrl: 'https://peaceful-meadow-44397.herokuapp.com/charitysearch?user_key=01289bdc6c2b9417e0d77a5dc3a34112&searchTerm=india',
+	//baseUrl: 'http://localhost:3005/charitysearch?user_key=01289bdc6c2b9417e0d77a5dc3a34112&searchTerm=india',
 
 	searchByName: function(searchTerm) {
 		return axios.get(this.baseUrl)
