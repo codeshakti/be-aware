ccc63bd74377fa901909f1354b71947a15362260
codeshakti <codeshaktiman@gmail.com>
2017-11-27 14:56:30 +0100

    json

--- server/api/data/UserData-demo.js
+++ server/api/data/UserData-demo.js
@@ -9,27 +9,27 @@ mongoose.connect(process.env.DB_URL, { useMongoClient: true })
 const userData = new(require('./UserData'))
 
 
-  userData.create("Jose", "Martinez","email@email.com", "Catalunya", "https://pbs.twimg.com/<profile_></profile_>images/616523971299799040/2TmsQKJa.jpg","5a189086cef92f2bbd0e20f3")
+//   userData.create("Jose", "Martinez","email@email.com", "Catalunya", "https://pbs.twimg.com/<profile_></profile_>images/616523971299799040/2TmsQKJa.jpg","5a189086cef92f2bbd0e20f3")
+//   	.then(console.log)
+//   	.catch(console.err)
+
+//  // userData.list()
+//  // 	.then(console.log)
+//  // 	.catch(console.err)
+
+//   // userData.retrieve('5a140779a5f91f45394be92b')
+//   // 	.then(console.log)
+//   // 	.catch(console.err)
+
+//   // userData.update("5a1820ceb7d6e85d17aea144","Jose", "Martinez", "email@email.com", "Catalunya", "https://pbs.twimg.com/profile_images/616523971299799040/2TmsQKJa.jpg","5a189086cef92f2bbd0e20f3","5a1a970ae384697478159623")
+//   // 	.then(console.log)
+//   // 	.catch(console.err)
+
+  userData.delete('5a1bed9f21ff6a43d27750e7')
   	.then(console.log)
   	.catch(console.err)
 
- // userData.list()
- // 	.then(console.log)
- // 	.catch(console.err)
-
-  // userData.retrieve('5a140779a5f91f45394be92b')
-  // 	.then(console.log)
-  // 	.catch(console.err)
-
-  // userData.update("5a1820ceb7d6e85d17aea144","Jose", "Martinez", "email@email.com", "Catalunya", "https://pbs.twimg.com/profile_images/616523971299799040/2TmsQKJa.jpg","5a189086cef92f2bbd0e20f3","5a1a970ae384697478159623")
-  // 	.then(console.log)
-  // 	.catch(console.err)
-
-  // userData.delete('5a18216c22617b5da5849fbd')
-  // 	.then(console.log)
-  // 	.catch(console.err)
-
-//  userData.create('buy milk', false)
-//      .then(task => userData.delete(task._id)
-//          .then(console.log))
-//      .catch(console.err)
+// //  userData.create('buy milk', false)
+// //      .then(task => userData.delete(task._id)
+// //          .then(console.log))
+// //      .catch(console.err)
--- server/db/README.md
+++ server/db/README.md
@@ -1,9 +1,17 @@
 ### From / to a json array file and a local server
 
 ```bash
-$ mongoimport -d <database-name> -c <collection-name> --drop --jsonArray --file <json-array-file>
+$ mongoimport -d beaware -c projects --drop --jsonArray --file data/projects.json
 
-$ mongoexport -d <database-name> -c <collection-name> --jsonArray -o <json-array-file>
+$ mongoexport -d beaware -c projects --jsonArray -o data/projects.json
+
+$ mongoimport -d beaware -c users --drop --jsonArray --file data/users.json
+
+$ mongoexport -d beaware -c users --jsonArray -o data/users.json
+
+$ mongoimport -d beaware -c orgs --drop --jsonArray --file data/orgs.json
+
+$ mongoexport -d beaware -c orgs --jsonArray -o data/orgs.json
 ```
 
 ### From / to a json array file and a remote mongo server
